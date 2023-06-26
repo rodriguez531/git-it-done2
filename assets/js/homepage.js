f034c54590873fb5dd1ca4ca73113ea5b408e778
rodriguez531 <alexr5408@gmail.com>
2023-06-26 16:00:57 -0400

    can search via languages now

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -58,7 +59,7 @@ var displayRepos = function(repos, searchTerm) {
     // format repo name
     var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-    // create a container for each repo
+    // create a link for each repo
     var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
     repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
@@ -90,5 +91,33 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
+var getFeaturedRepos = function(language){
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+  fetch(apiUrl).then(function(response){
+    if (response.ok){
+      response.json().then(function(data){
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert('Error: Github User Not Found');
+    }
+  });
+};
+
+var buttonClickHandler = function(event){
+  var language = event.target.getAttribute("data-language");
+  
+  if (language){
+    getFeaturedRepos(language);
+
+  //clear old content
+    repoContainerEl.textContent = "";
+  }
+}
+
+
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
--- assets/js/single.js
+++ assets/js/single.js
@@ -23,22 +23,22 @@ var getRepoIssues = function(repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
   // make a get request to url
-fetch(apiUrl).then(function(response) {
-  // request was successful
-  if (response.ok) {
-    response.json().then(function(data) {
-      displayIssues(data);
+  fetch(apiUrl).then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayIssues(data);
 
-      // check if api has paginated issues
-      if (response.headers.get("Link")) {
-        displayWarning(repo);
-      }
-    });
-  } else {
-    // if not successful, redirect to homepage
-    document.location.replace("./index.html");
-  }
-});
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
+      });
+    } else {
+      // if not successful, redirect to homepage
+      document.location.replace("./index.html");
+    }
+  });
 };
 
 var displayIssues = function(issues) {
@@ -54,12 +54,11 @@ var displayIssues = function(issues) {
     issueEl.classList = "list-item flex-row justify-space-between align-center";
     issueEl.setAttribute("href", issues[i].html_url);
     issueEl.setAttribute("target", "_blank");
-    
+
     // create span to hold issue title
     var titleEl = document.createElement("span");
     titleEl.textContent = issues[i].title;
-    
-  
+
     // append to container
     issueEl.appendChild(titleEl);
 
@@ -69,14 +68,13 @@ var displayIssues = function(issues) {
     // check if issue is an actual issue or a pull request
     if (issues[i].pull_request) {
       typeEl.textContent = "(Pull request)";
-    }
-    else {
+    } else {
       typeEl.textContent = "(Issue)";
     }
 
     // append to container
     issueEl.appendChild(typeEl);
-  
+
     // append to the dom
     issueContainerEl.appendChild(issueEl);
   }
@@ -95,6 +93,5 @@ var displayWarning = function(repo) {
   // append to warning container
   limitWarningEl.appendChild(linkEl);
 };
-
 
 getRepoName();
